5c64df0d47783b60f3bea4e6fcc8a66bfbf5a499
JeremyFlowers <rpgjuwangaming@gmail.com>
2021-08-19 12:57:15 -0500

    Created a collision check loop

--- ProjectPhysics/PhysicsScene.cpp
+++ ProjectPhysics/PhysicsScene.cpp
@@ -1,5 +1,6 @@
 #include "PhysicsScene.h"
 #include "PhysicsObject.h"
+#include "Sphere.h"
 
 PhysicsScene::PhysicsScene() : m_timeStep(0.01f), m_gravity(glm::vec2(0,0))
 {
@@ -33,6 +34,18 @@ void PhysicsScene::update(float deltaTime)
 			actor->fixedUpdate(m_gravity, m_timeStep);
 		}
 		accumulatedTime -= m_timeStep;
+
+		for (auto outer = m_actors.begin(); outer != --m_actors.end(); outer++)
+		{
+			for (auto inner = ++outer; inner != m_actors.end(); inner++)
+			{
+				PhysicsObject* object1 = *outer;
+				PhysicsObject* object2 = *inner;
+
+				//Collision check
+				sphereToSphere(dynamic_cast<Sphere*>(object1), dynamic_cast<Sphere*>(object2));
+			}
+		}
 	}
 }
 
@@ -42,4 +55,9 @@ void PhysicsScene::draw()
 	{
 		actor->draw();
 	}
+}
+
+bool PhysicsScene::sphereToSphere(Sphere* sphere1, Sphere* sphere2)
+{
+	return false;
 }
