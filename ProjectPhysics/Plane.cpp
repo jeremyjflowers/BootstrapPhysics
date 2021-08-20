8c6c61d7d39b3330586199177b7baf81aa5bb976
JeremyFlowers <rpgjuwangaming@gmail.com>
2021-08-20 15:58:25 -0500

    Removed the colorFade code from Plane.cpp

--- ProjectPhysics/Plane.cpp
+++ ProjectPhysics/Plane.cpp
@@ -21,8 +21,6 @@ void Plane::draw()
 	float linesegmentLength = 300.0f;
 	glm::vec2 centerPoint = m_normal * m_distance;
 	glm::vec2 parallel(m_normal.y -m_normal.x);
-	glm::vec4 colorFade = m_color;
-	colorFade.a = 0.0f;
 
 	glm::vec2 start = centerPoint + (parallel * linesegmentLength);
 	glm::vec2 end = centerPoint - (parallel * linesegmentLength);
