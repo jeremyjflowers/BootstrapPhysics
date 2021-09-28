b99390e9b149c01d6e286431a44239ee4a93f3ca
Jeremy Flowers <rpgjuwangaming@gmail.com>
2021-09-28 15:04:38 -0500

    Add an Engine constructor
    Still have 10 linker errors

--- GraphicsProject/Engine.h
+++ GraphicsProject/Engine.h
@@ -1,19 +1,26 @@
 #pragma once
+
+struct GLFWwindow;
+
 class Engine
 {
 public:
 	Engine();
+	Engine(int width, int height, const char* title);
 	~Engine();
 
 	int run();
 private:
 	int start();
 	int update();
+	int draw();
 	int end();
 
 	bool getGameOver();
 
 private:
-
+	GLFWwindow* m_window = nullptr;
+	int m_width, m_height;
+	const char* m_title;
 };
 
